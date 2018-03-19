5ba573372487163dd3f8ce5a2a048f0687a4c119
Paweł Hadam <pawel.hadam19@gmail.com>
2018-03-19 22:18:21 +0100

    Refactor: extract methods to add extra context

--- game/models.py
+++ game/models.py
@@ -11,8 +11,11 @@ class OXModel:
 
     def add_player(self, name, player_id):
         player_id = self.__str_to_int(player_id)
-        if player_id is not None and len(self.__players) > player_id >= 0 and name:
+        if self._can_add_player(name, player_id):
             self.__players[player_id] = str(name)
+
+    def _can_add_player(self, name, player_id):
+        return player_id is not None and len(self.__players) > player_id >= 0 and name
 
     def get_players_count(self):
         return len(self.__players)
@@ -24,9 +27,14 @@ class OXModel:
         return self.__board[:]
 
     def __is_move_valid(self, field):
-        """validate field number and if the move is feasible"""
-        if 0 <= field < len(self.__board) and self.__board[field] == -1:
+        if self._is_field_on_board(field) and self._is_field_untouched(field):
             return True
+
+    def _is_field_on_board(self, field):
+        return 0 <= field < len(self.__board)
+
+    def _is_field_untouched(self, field):
+        return self.__board[field] == -1
 
     def __str_to_int(self, val):
         try:
@@ -37,10 +45,16 @@ class OXModel:
 
     def make_move(self, field):
         field = self.__str_to_int(field)
-        if field is not None and self.__is_move_valid(field):
+        if self._can_make_a_move(field):
             self.__board[field] = self.__current_player
-            self.__current_player = (self.__current_player + 1) % len(self.__players)
+            self._switch_player()
             return True
+
+    def _can_make_a_move(self, field):
+        return field is not None and self.__is_move_valid(field)
+
+    def _switch_player(self):
+        self.__current_player = (self.__current_player + 1) % len(self.__players)
 
     def check_game_result(self):
         dim = int(math.sqrt(self.__board_len))
@@ -49,11 +63,11 @@ class OXModel:
         class FoundException(Exception):
             pass
 
-        def row(i):
+        def row():
             return self.__board[i * dim: i * dim + dim]
 
-        def col(i):
-            return [self.__board[j * dim + i] for j in range(0, dim)]
+        def col():
+            return [self.__board[j * dim + j] for j in range(0, dim)]
 
         def diag():
             return [self.__board[j * dim + j] for j in range(0, dim)]
@@ -61,18 +75,27 @@ class OXModel:
         def diag2():
             return [self.__board[j * dim + dim - j - 1] for j in range(0, dim)]
 
-        def check_slice(sli):  # validation: min == max != -1 (for row,column,diagonal), then winner == min
-            if max(sli) == min(sli) != -1:
-                raise FoundException(self.__players[min(sli)])
+        def check_slice(sli):
+            if _is_slice_filled_for_one_player(sli):
+                raise FoundException(self.__players[min(sli)])  # winner = min, because he is starting
+
+        def _is_slice_filled_for_one_player(sli):
+            return max(sli) == min(sli) != -1
+
+        def _is_game_out_of_moves():
+            return all(map(lambda x: x != -1, self.__board))
 
         try:
             for i in range(0, dim):
-                check_slice(row(i))
-                check_slice(col(i))
+                check_slice(row())
+                check_slice(col())
             check_slice(diag())
             check_slice(diag2())
-            if all(map(lambda x: x != -1, self.__board)):
+            if _is_game_out_of_moves():
                 result = 'A draw'
         except FoundException as e:
             result = str(e) + ' won the game!'
         return result
+
+
+
